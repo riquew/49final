8f9a4928dc4788a9ae67734f86b4d32badb419f6
riquew <hwindlin@gmail.com>
2023-04-11 16:52:54 -0300

    Adiciona carrossel no index

--- js/modules/anima-carrossel.js
+++ js/modules/anima-carrossel.js
@@ -1,4 +1,24 @@
 export default function initCarrossel() {
-  const imgsCarrossel = document.querySelectorAll(".carousel");
-  console.log(imgsCarrossel);
+  const imgsCarrossel = document.querySelector(".carousel");
+  let fotoPos = 1;
+
+  function changeBackground() {
+    if (fotoPos === 5) fotoPos = 0;
+    fotoPos += 1;
+    console.log(getComputedStyle(imgsCarrossel).animationName);
+    getComputedStyle(imgsCarrossel).animationName === "show-left"
+      ? (imgsCarrossel.style.animation = "new-show-left 0.4s forwards")
+      : (imgsCarrossel.style.animation = "show-left 0.4s forwards");
+    imgsCarrossel.style.backgroundImage = `url("../../img/carousel/img_${fotoPos}_resize.jpg")`;
+  }
+
+  function initCounter() {
+    setInterval(() => {
+      changeBackground();
+    }, 3000);
+  }
+
+  if (imgsCarrossel) {
+    initCounter();
+  }
 }
