28ba3e5d570705e331edc6bc600cd4d8cdc1da7c
riquew <hwindlin@gmail.com>
2023-04-26 09:25:30 -0300

    Finaliza anima mapa

---
+++ js/modules/anima-mapa.js
@@ -0,0 +1,47 @@
+export default function initAnimaMapa() {
+  const observer = new MutationObserver(handleMutation);
+  const observerTarget = document.querySelector(".mapa");
+  const cias = Array.from(document.querySelectorAll(".mapa-cias h2"));
+  const subTitles = Array.from(document.querySelectorAll(".mapa-cias article"));
+
+  function addAnimation(item, size, properties) {
+    let i = 0;
+    const animation = setInterval(() => {
+      console.log(item[i]);
+      item[i].classList.add(properties.animation);
+      if (i === size) {
+        clearInterval(animation);
+      }
+      i++;
+    }, properties.counter);
+  }
+
+  function verifySize(item) {
+    const size = item.length - 1;
+    const properties =
+      item === cias
+        ? {
+            counter: 400,
+            animation: "fadeRight",
+          }
+        : {
+            counter: 300,
+            animation: "fadeDown",
+          };
+    addAnimation(item, size, properties);
+  }
+
+  function handleMutation() {
+    observer.disconnect();
+    verifySize(cias);
+    setTimeout(verifySize, 1000, subTitles);
+  }
+
+  function addMutationObserver() {
+    observer.observe(observerTarget, { attributes: true });
+  }
+
+  if (observerTarget) {
+    addMutationObserver();
+  }
+}
