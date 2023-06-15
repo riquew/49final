fd43daf57ae6d6f03cab16845cce807c30bcf510
riquew <hwindlin@gmail.com>
2023-06-15 10:49:49 -0300

    Corrige navegacao dropdown menu

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,11 +1,12 @@
 import outsideClick from "./outsideClick.js";
 
 export default function initDropDownMenu() {
-  const itensNav = document.querySelectorAll("[data-dropdown]");
+  const itensNav = document.querySelectorAll("[data-dropdown] a");
 
   function showDropDownMenu(event) {
-    event.preventDefault();
-    const dropDownMenu = this.querySelector("ul");
+    if (event.currentTarget.classList.value === "subNav")
+      event.preventDefault();
+    const dropDownMenu = this.parentElement.querySelector("ul");
     dropDownMenu.classList.add("ativo");
     outsideClick(dropDownMenu, ["touchstart", "click"], () => {
       dropDownMenu.classList.remove("ativo");
@@ -14,7 +15,7 @@ export default function initDropDownMenu() {
 
   if (itensNav) {
     itensNav.forEach((item) =>
-      ["click", "touchstart"].forEach((userEvent) => {
+      ["click"].forEach((userEvent) => {
         item.addEventListener(userEvent, showDropDownMenu);
       })
     );
