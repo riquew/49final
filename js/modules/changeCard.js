c23f5fadea3f8bbb0ef04d0274f74b1d56953734
riquew <hwindlin@gmail.com>
2023-06-16 12:01:06 -0300

    Entrega subUnidades

--- js/modules/changeCard.js
+++ js/modules/changeCard.js
@@ -1,12 +1,37 @@
+import data from "../data/dadosSubUnidades.json" assert { type: "json" };
+
 export default function initChangeCard() {
   const fotoSecundarias = document.querySelectorAll(".fotoSecundaria img");
   const fotoPrincipal = document.querySelector(".fotoPrincipal img");
+  const cardTexto = document.querySelector(".cardTexto");
+  const dados = data;
 
   function changeCard(event) {
-    let fotoTemp = fotoPrincipal.src;
-    // this.src = fotoPrincipal.src;
+    checkAnimation(fotoPrincipal);
+    let fotoTemp = {
+      src: fotoPrincipal.src,
+      id: fotoPrincipal.id,
+    };
     fotoPrincipal.src = event.currentTarget.src;
-    this.src = fotoTemp;
+    fotoPrincipal.id = event.currentTarget.id;
+    this.src = fotoTemp.src;
+    this.id = fotoTemp.id;
+    changeText(fotoPrincipal.id);
+  }
+
+  function changeText(id) {
+    checkAnimation(cardTexto);
+    cardTexto.querySelector("h2").innerText = dados[id].titulo;
+    cardTexto.querySelector(".endereco").innerText = dados[id].endereco;
+    cardTexto.querySelector(".municipio").innerText = dados[id].municipio;
+    cardTexto.querySelector(".telefone").innerText = dados[id].telefone;
+    cardTexto.querySelector(".email").innerText = dados[id].email;
+  }
+
+  function checkAnimation(foto) {
+    getComputedStyle(foto).animationName === "show-down"
+      ? (foto.style.animation = "new-show-down 0.4s forwards")
+      : (foto.style.animation = "show-down 0.4s forwards");
   }
 
   if (fotoSecundarias) {
